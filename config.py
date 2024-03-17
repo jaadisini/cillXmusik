b5f75c60bd93cda2fb295d7955b94c370a31d4ab
jaadisini <162589971+jaadisini@users.noreply.github.com>
2024-03-17 13:49:13 +0700

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://xnxx.com",
+    "https://github.com/jaadisini/cillXmusik",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
